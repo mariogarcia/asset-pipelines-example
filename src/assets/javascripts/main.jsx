bdf6da4985580730064f8cb9067b7b7569f262af
Mario Garcia <mario.ggar@gmail.com>
2022-12-07 22:23:53 +0100

    chore: refactoring files

--- src/assets/javascripts/main.jsx
+++ src/assets/javascripts/main.jsx
@@ -1,25 +1,6 @@
 //= require react.development.js
 //= require react-dom.development.js
-//= require_tree .
-
-class Button extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
-  }
-
-  render() {
-    if (this.state.liked) {
-      return 'You liked this.';
-    }
-
-    return (
-        <button onClick={() => this.setState({ liked: true })}>
-          Like
-        </button>
-    )
-  }
-}
+//= require components/button.js
 
 const root = ReactDOM.createRoot(document.getElementById("app"))
 root.render(<Button />);
