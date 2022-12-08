c1c6a3b699e4493dd78d4dab52dfb76c898ed855
Mario Garcia <mario.ggar@gmail.com>
2022-12-08 19:43:16 +0100

    chore: formatting button component

--- src/assets/javascripts/layout/menu.tsx
+++ src/assets/javascripts/layout/menu.tsx
@@ -1,16 +1,13 @@
 class Menu extends React.Component {
     render() {
         return (
-
-              <div className="home-menu pure-menu pure-menu-horizontal pure-menu-fixed">
-                  <a className="pure-menu-heading" href="#">Assets-Pipelines</a>
-
-                  <ul className="pure-menu-list">
-                      <li className="pure-menu-item pure-menu-selected"><a href="index.html" className="pure-menu-link">Home</a></li>
-                      <li className="pure-menu-item"><a href="about.html" className="pure-menu-link">About</a></li>
-                  </ul>
-              </div>
-
+          <div className="home-menu pure-menu pure-menu-horizontal pure-menu-fixed">
+              <a className="pure-menu-heading" href="#">Assets-Pipelines</a>
+              <ul className="pure-menu-list">
+                  <li className="pure-menu-item pure-menu-selected"><a href="index.html" className="pure-menu-link">Home</a></li>
+                  <li className="pure-menu-item"><a href="about.html" className="pure-menu-link">About</a></li>
+              </ul>
+          </div>
         )
     }
 }
